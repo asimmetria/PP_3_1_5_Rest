3aca2c80f4ca0aed85d57a035a2565a4b727d31b
Mikhail Altunin <altunin@list.ru>
2023-05-10 13:26:11 +0300

    final version.11

--- src/main/resources/static/js/admin/allRoles.js
+++ src/main/resources/static/js/admin/allRoles.js
@@ -1,5 +1,7 @@
 const getRolesUrl = '/api/roles'
 
 function getRolesRequest() {
-    return fetch(getRolesUrl).then(response => response.json())
+    return fetch(getRolesUrl)
+        .then(response => response.json())
+        .catch(err => console.error(err))
 }
